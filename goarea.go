77e88af51135b0474965c6ee4f6df795af5b0b32
gjcc-br <gustavojccamara@gmail.com>
2020-01-01 09:26:53 -0300

    Atualizacao do arquivo

--- goarea.go
+++ goarea.go
@@ -7,7 +7,7 @@ const Pi = 3.1416
 
 //Circ eh responsavel por calcular a area da circunferencia
 func Circ(raio float64) float64 {
-	return math.Pow(raio, 2) * Pi
+	return math.Pow(raio, 4) * Pi
 }
 
 //Rect eh responsavel por calcular a area de um quadrado
